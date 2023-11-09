f7603310675ba0ad93e9739a8bd25054e00584d5
Hyeonjoon <kingpark02@naver.com>
2023-11-09 16:15:47 +0900

    14467번 문제 수정

--- src/implementation/_14467.java
+++ src/implementation/_14467.java
@@ -11,11 +11,11 @@ public class _14467 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        int[][] arr = new int[11][1];
+        int[] arr = new int[11];
         int cow_sum = 0;
 
         for(int i=0; i<11; i++) {
-            arr[i][0] = -1;
+            arr[i] = -1;
         }
 
         StringTokenizer st;
@@ -23,15 +23,8 @@ public class _14467 {
             st = new StringTokenizer(br.readLine(), " ");
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-
-            if(arr[a][0] == -1) {
-                arr[a][0] = b;
-            } else {
-                if(arr[a][0] != -1) {
-                    cow_sum++;
-                    arr[a][0] = b;
-                }
-            }
+            if(arr[a] != -1 && arr[a] != b) cow_sum++;
+            arr[a] = b;
         }
         System.out.println(cow_sum);
     }
