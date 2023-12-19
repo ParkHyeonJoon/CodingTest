5a8a59ae212732026c295e2fb3089bc6b54f21d4
Hyeonjoon <kingpark02@naver.com>
2023-12-20 03:49:47 +0900

    고급 알고리즘 14주차/구간 합 구하기

---
+++ src/final_test/_14week.java
@@ -0,0 +1,21 @@
+package final_test;
+
+import java.util.Scanner;
+
+public class _14week {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int q = sc.nextInt();
+        int[] arrS = new int[n+1];
+        for(int i=1; i<=n; i++) {
+            arrS[i] = arrS[i-1] + sc.nextInt();
+        }
+
+        for(int i=0; i<q; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(arrS[b] - arrS[a-1]);
+        }
+    }
+}
