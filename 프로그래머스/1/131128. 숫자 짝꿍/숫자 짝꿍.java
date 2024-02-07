772da61d63f2a4105abad4fe76674c63c1a26078
parkhyeonjoon <97442408+ParkHyeonJoon@users.noreply.github.com>
2024-02-08 00:17:13 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.33 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String X, String Y) {
+        int[] xArr = new int[10];
+        int[] yArr = new int[10];
+        StringBuilder sb = new StringBuilder();
+
+        for(String x : X.split("")) {
+            xArr[Integer.parseInt(x)]++;
+        }
+
+        for(String y : Y.split("")) {
+            yArr[Integer.parseInt(y)]++;
+        }
+
+        for(int i=9; i>=0; i--) {
+            while(xArr[i] > 0 && yArr[i] > 0) {
+                sb.append(i);
+                xArr[i]--;
+                yArr[i]--;
+            }
+        }
+        String answer = "";
+        
+        if(sb.toString().equals("")) answer = "-1";
+        else if(sb.toString().substring(0, 1).equals("0")) answer = "0";
+        else answer = sb.toString();
+        
+        return answer;
+    }
+}
