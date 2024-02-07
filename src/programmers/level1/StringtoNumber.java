2b782aca179dbf83772540f81e8c414110f6670b
Hyeonjoon <kingpark02@naver.com>
2024-02-07 14:43:07 +0900

    Lv.1 문자열을 정수로

---
+++ src/programmers/level1/StringtoNumber.java
@@ -0,0 +1,11 @@
+package programmers.level1;
+
+class StringtoNumber {
+    public int solution(String s) {
+        int answer = 0;
+        answer = Integer.parseInt(s);
+        return answer;
+    }
+}
+
+
