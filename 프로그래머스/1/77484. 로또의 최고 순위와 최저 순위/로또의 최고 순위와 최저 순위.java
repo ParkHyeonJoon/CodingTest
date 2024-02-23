bbb122703adaa4117418946fe7117d01dd22069e
parkhyeonjoon <97442408+ParkHyeonJoon@users.noreply.github.com>
2024-02-23 14:09:54 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.02 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -1,15 +1,9 @@
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
-        /*
-        lottos[]과 win_nums[] 배열을 비교해서 같다면 win_count++;
         
-        lottos[]에서 0이 몇개인지 찾아서 그 수만큼 win_count에 더해줌
-        
-        안 더한 win_count와 더한 win_count를 배열로 출력
-        */
         int winCount = 0; int zeroCount = 0;
-        for(int i=0; i<6; i++) {
-            for(int j=0; j<6; j++) {
+        for(int i=0; i<lottos.length; i++) {
+            for(int j=0; j<win_nums.length; j++) {
                 if(lottos[i] == win_nums[j]) {
                     winCount++;
                 }
