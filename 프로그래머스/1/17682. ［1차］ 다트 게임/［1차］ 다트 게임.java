34713b6019f68030acdfff29637336fb495bd907
parkhyeonjoon <97442408+ParkHyeonJoon@users.noreply.github.com>
2024-03-13 04:27:51 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.04 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -1,48 +1,58 @@
-import java.util.*; 
+import java.util.*;
 
 class Solution {
     public int solution(String dartResult) {
         int index = 0;
         int[] num = new int[3];
-        
-        for(int i=0; i<dartResult.length(); i++) {
+
+        for (int i = 0; i < dartResult.length(); i++) {
             char c = dartResult.charAt(i);
-            if(48 == c) {
-                if(i==0) {
-                    num[index] = Character.getNumericValue(c);
-                    index++;
-                    continue;
-                } else {
-                    char beforeC = dartResult.charAt(i-1);
-                    if(48 < beforeC && beforeC <= 57) {
-                        beforeC += '0';
-                        num[index-1] = Character.getNumericValue(beforeC);
-                        continue;
-                    } else {
-                        num[index] = Character.getNumericValue(c);
-                        index++;
-                        continue;
-                    }
-                }
+
+            if (Character.isDigit(c)) {
+                index = processDigit(c, i, index, num, dartResult);
+            } else {
+                processSymbol(c, index, num);
             }
-            if(48 < c && c <= 57) {
-                num[index] = Character.getNumericValue(c);
-                index++;
-                continue;
-            }
-            if(c == 'S') num[index-1] = num[index-1] * 1;
-            if(c == 'D') num[index-1] = num[index-1] * num[index-1];
-            if(c == 'T') num[index-1] = num[index-1] * num[index-1] * num[index-1];
-            if(c == '*') {
-                if(index == 1) num[index-1] = num[index-1] * 2;
-                if(index > 1) {
-                    num[index-2] = num[index-2] * 2; 
-                    num[index-1] = num[index-1] * 2;
-                }
-            }
-            if(c == '#') num[index-1] = num[index-1] * -1;
-        
         }
+
         return num[0] + num[1] + num[2];
+    }
+
+    private int processDigit(char c, int i, int index, int[] num, String dartResult) {
+        int digit = Character.getNumericValue(c);
+
+        if (index > 0 && Character.isDigit(dartResult.charAt(i - 1))) {
+            num[index - 1] = num[index - 1] * 10;
+        } else {
+            num[index++] = digit;
+        }
+
+        return index;
+    }
+
+    private void processSymbol(char c, int index, int[] num) {
+        switch (c) {
+            case 'S':
+                num[index - 1] = num[index - 1] * 1;
+                break;
+            case 'D':
+                num[index - 1] = num[index - 1] * num[index - 1];
+                break;
+            case 'T':
+                num[index - 1] = num[index - 1] * num[index - 1] * num[index - 1];
+                break;
+            case '*':
+                if (index == 1) {
+                    num[index - 1] = num[index - 1] * 2;
+                }
+                if (index > 1) {
+                    num[index - 2] = num[index - 2] * 2;
+                    num[index - 1] = num[index - 1] * 2;
+                }
+                break;
+            case '#':
+                num[index - 1] = num[index - 1] * -1;
+                break;
+        }
     }
 }
