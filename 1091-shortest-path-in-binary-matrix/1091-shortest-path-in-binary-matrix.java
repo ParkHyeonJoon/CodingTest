5711224e3fa97cb17138bbf11953a7740e27c0b4
parkhyeonjoon <97442408+ParkHyeonJoon@users.noreply.github.com>
2024-03-29 18:26:16 +0900

    Time: 15 ms (75.33%), Space: 45 MB (87.62%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -2,17 +2,17 @@ import java.util.*;
 
 public class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
-        if (grid[0][0] == 1 || grid[grid.length - 1][grid[0].length - 1] == 1) {
+        int n = grid.length;
+        if (grid[0][0] == 1 || grid[n-1][n-1] == 1) {
             return -1; 
         }
 
         int[][] directions = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
-        int n = grid.length;
         boolean[][] visited = new boolean[n][n];
         Queue<int[]> queue = new LinkedList<>();
         queue.offer(new int[]{0, 0});
         visited[0][0] = true;
-        int pathLength = 1;
+        int count = 1;
 
         while (!queue.isEmpty()) {
             int size = queue.size();
@@ -21,21 +21,20 @@ public class Solution {
                 int row = current[0];
                 int col = current[1];
                 if (row == n - 1 && col == n - 1) {
-                    return pathLength; 
+                    return count; 
                 }
                 for (int[] dir : directions) {
-                    int newRow = row + dir[0];
-                    int newCol = col + dir[1];
-                    if (newRow >= 0 && newRow < n && newCol >= 0 && newCol < n && !visited[newRow][newCol] && grid[newRow][newCol] == 0) {
-                        queue.offer(new int[]{newRow, newCol});
-                        visited[newRow][newCol] = true;
+                    int nextRow = row + dir[0];
+                    int nextCol = col + dir[1];
+                    if (nextRow >= 0 && nextRow < n && nextCol >= 0 && nextCol < n && !visited[nextRow][nextCol] && grid[nextRow][nextCol] == 0) {
+                        queue.offer(new int[]{nextRow, nextCol});
+                        visited[nextRow][nextCol] = true;
                     }
                 }
             }
-            pathLength++;
+            count++;
         }
 
         return -1; 
     }
-
 }
