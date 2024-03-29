34974e0db65668657d85f4b4e3262b9e63e23229
parkhyeonjoon <97442408+ParkHyeonJoon@users.noreply.github.com>
2024-03-29 15:10:52 +0900

    Time: 1 ms (78.08%), Space: 44.1 MB (70.32%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -1,14 +1,25 @@
+import java.util.*; 
+
 class Solution {
-    Map<Integer, List<Integer>> graph = new HashMap<>();
+    // Map<Integer, List<Integer>> graph = new HashMap<>();
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-        for(int i=0; i<rooms.size(); i++) {
-            graph.put(i, rooms.get(i));
-        }
+//         for(int i=0; i<rooms.size(); i++) {
+//             graph.put(i, rooms.get(i));
+//         }
+//         boolean[] visited = new boolean[rooms.size()];
+        
+//         dfs(graph, visited, 0);
+        
+//         for(int i=0; i<rooms.size(); i++) {
+//             if(!visited[i]) {
+//                 return false;
+//             }
+//         }
+//         return true;
+        
         boolean[] visited = new boolean[rooms.size()];
-        
-        dfs(graph, visited, 0);
-        
-        for(int i=0; i<rooms.size(); i++) {
+        bfs(rooms, visited, 0);
+        for(int i=0; i<visited.length; i++) {
             if(!visited[i]) {
                 return false;
             }
@@ -16,11 +27,27 @@ class Solution {
         return true;
     }
     
-    public void dfs(Map<Integer, List<Integer>> graph, boolean[] visited, int start) {
+    // public void dfs(Map<Integer, List<Integer>> graph, boolean[] visited, int start) {
+    //     visited[start] = true;
+    //     for(int nextX : graph.get(start)) {
+    //         if(!visited[nextX]) {
+    //             dfs(graph, visited, nextX);
+    //         }
+    //     }
+    // }
+    
+    public void bfs(List<List<Integer>> rooms, boolean[] visited, int start) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(start);
         visited[start] = true;
-        for(int nextX : graph.get(start)) {
-            if(!visited[nextX]) {
-                dfs(graph, visited, nextX);
+        
+        while(!queue.isEmpty()) {
+            int curX = queue.poll();
+            for(int nextX : rooms.get(curX)) {
+                if(!visited[nextX]) {
+                    queue.offer(nextX);
+                    visited[nextX] = true;
+                }
             }
         }
     }
