7906773e3c77e641c167ece2a3175ea2c70c77a7
parkhyeonjoon <97442408+ParkHyeonJoon@users.noreply.github.com>
2024-04-22 17:42:49 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.02 ms, Memory: 65.4 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -1,17 +1,14 @@
 class Solution {
     public int solution(int storey) {
         int answer = 0;
-        // storey를 10으로 나눴을 때의 나머지를 remainNum
-        // remainNum이 5보다 작거나 같으면 remainNum을 answer에 +, storey = storey-remainNum
-        // remainNum이 6보다 크거나 같으면 10 - remainNum을 answer에 +, storey = storey + (10-remainNum)
-        // storey >= 10 이면  
+        
         while(storey > 0) {
             int remainNum = storey % 10;
             storey = storey / 10;
             
             if(remainNum == 5) {
                 if(storey % 10 >= 5) {
-                    answer = answer + (10 - remainNum);
+                    answer = answer + remainNum;
                     storey++;
                 } else {
                     answer = answer + remainNum;
