1a0a90d8e1a5348aa3ead350bbf3644a582f1e82
parkhyeonjoon <97442408+ParkHyeonJoon@users.noreply.github.com>
2024-05-20 18:01:55 +0900

    [level 2] Title: 행렬의 곱셈, Time: 2.36 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -6,7 +6,6 @@ class Solution {
         int n = arr1[0].length;     // arr1의 열 수이자 arr2의 행 수
         int p = arr2[0].length;     // arr2의 열 수
 
-        // 결과 행렬 초기화
         int[][] result = new int[m][p];
 
         // 행렬 곱셈 수행
