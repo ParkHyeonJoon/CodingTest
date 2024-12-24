6267a49398b4563b3cba57aff8bc93e7cb983383
parkhyeonjoon <97442408+ParkHyeonJoon@users.noreply.github.com>
2024-12-25 00:35:11 +0900

    [Bronze I] Title: 진법 변환 2, Time: 176 ms, Memory: 17804 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -11,35 +11,22 @@ public class Main {
     }
 
     static void pro() {
-        Stack<String> stack = new Stack<>();
-        while(N >= B) {
-            int remain = N%B; // 나머지, B진법으로 전환한 수에 들어가야 하는 값
-            if(remain > 9) {
-                stack.push(changeNumToChar(remain));
-            } else {
-                stack.push(String.valueOf(remain));
-            }
-            N /= B; // 몫, 다음에 나눌 값
-        }
-        
-        if(N > 9) {
-            stack.push(changeNumToChar(N));
-        } else {
-            stack.push(String.valueOf(N));
-        }
-
         StringBuilder sb = new StringBuilder();
-        while(!stack.isEmpty()) {
-            sb.append(stack.pop());
+        
+        while (N > 0) {
+            int remain = N % B; // 나머지 계산
+            if (remain > 9) {
+                sb.append((char) (remain - 10 + 'A')); // 알파벳으로 변환
+            } else {
+                sb.append(remain); // 숫자로 변환
+            }
+            N /= B; // 몫 계산
         }
 
-        System.out.print(sb.toString());
+        // 최종 결과 뒤집기
+        System.out.print(sb.reverse().toString());
     }
 
-    static String changeNumToChar(int remain) {
-        char c = (char) (remain - 10 + 'A');
-        return String.valueOf(c);
-    }
     public static void main(String[] args) {
         input();
         pro();
