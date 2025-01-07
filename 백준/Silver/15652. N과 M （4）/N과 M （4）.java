b980804213a8e60fd3aba44aba5c0c22a7a03701
parkhyeonjoon <97442408+ParkHyeonJoon@users.noreply.github.com>
2025-01-07 11:28:38 +0900

    [Silver III] Title: N과 M (4), Time: 140 ms, Memory: 16516 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -1,43 +1,39 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
     static int N, M;
-    static int[] selected, used;
+    static int[] selected;
     static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) {
-        input();
-
-        rec_func(1);
-        System.out.println(sb.toString());
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        N = Integer.parseInt(input[0]);
+        M = Integer.parseInt(input[1]);
+        selected = new int[M+1];
+        selected[0] = 1;
     }
-
-    // Recurrence Funtion(재귀함수)
-    // 만약 M개를 전부 고름 -> 조건에 맞는 탐색을 한 가지 성공한 것!
-    // 아직 M개를 고르지 않음 -> k 번째부터 M번째 원소를 조건에 맞게 고르는 모든 방법을 시도한다.
-    static void rec_func(int k) {
-        if(k == M + 1) { // 다 골랐다!
-            // selected[1..M] 배열이 새롭게 탐색된 결과
-            for(int i=1; i<=M; i++) sb.append(selected[i]).append(' ');
+    static void rec(int k) {
+        // Base case
+        if(k == M+1) {
+            for(int i=1; i<=M; i++) sb.append(selected[i]).append(" ");
             sb.append('\n');
-        } else {
-            int start = selected[k-1];
-            if(start == 0) start = 1;
-            for(int cand=start; cand <=N; cand++) {
+        }
+        // Recursion
+        else {
+            for(int cand=selected[k-1]; cand<=N; cand++) {
                 selected[k] = cand;
-
-                // k+1번 ~ M번 을 모두 탐색하는 일을 해야 하는 상황
-                rec_func(k+1);
+                rec(k+1);
                 selected[k] = 0;
             }
         }
     }
 
-    static void input() {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
-        selected = new int[M + 1];
+    public static void main(String[] args) throws IOException {
+        input();
+        rec(1);
+        System.out.println(sb.toString());
     }
 }
