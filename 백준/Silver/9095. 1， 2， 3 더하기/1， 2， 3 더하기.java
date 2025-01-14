dab8cbeaff8fc8b1ccc6b984c09c4ee3e5e2c528
parkhyeonjoon <97442408+ParkHyeonJoon@users.noreply.github.com>
2025-01-14 13:28:05 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 104 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,32 +1,41 @@
+import java.io.*;
 import java.util.*;
-public class Main {
-    static StringBuilder sb = new StringBuilder();
-    static int[] Dy;
 
-    static void preprocess() {
-        Dy = new int[15];
-        
-        Dy[1] = 1;
-        Dy[2] = 2;
-        Dy[3] = 4;
-        
-        for(int i=4; i<=11; i++) {
-            Dy[i] = Dy[i-1] + Dy[i-2] + Dy[i-3];
+public class Main {
+    static int T;
+    static int[] dp;
+
+    static int[] arr;
+
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        T = Integer.parseInt(br.readLine());
+        dp = new int[11];
+        arr = new int[T];
+        for(int i=0; i<T; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+    }
+
+    static void dynamic() {
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        for(int i=4; i<=10; i++) {
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
         }
     }
 
     static void pro() {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        for(int i=1; i<=T; i++) {
-            int N = sc.nextInt();
-            sb.append(Dy[N]).append('\n');
+        for(int i : arr) {
+            System.out.println(dp[i]);
         }
-        System.out.println(sb);
     }
 
-    public static void main(String[] args) {
-        preprocess();
+
+    public static void main(String[] args) throws IOException {
+        input();
+        dynamic();
         pro();
     }
 }
