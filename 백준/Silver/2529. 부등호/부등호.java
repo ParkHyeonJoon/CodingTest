b1a97403d5635b6cdc6ad9f02833800a7b71db14
parkhyeonjoon <97442408+ParkHyeonJoon@users.noreply.github.com>
2025-01-20 17:08:53 +0900

    [Silver I] Title: 부등호, Time: 152 ms, Memory: 20276 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.java
+++ 백준/Silver/2529. 부등호/부등호.java
@@ -1,70 +1,65 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.io.IOException;
+import java.io.*;
+import java.util.*;
 
- 
 public class Main {
+   static int k;
+   static char[] inequality;
+   static boolean[] visited;
+   static int[] arr;
+   static String min = "9999999999";
+   static String max = "0";
 
-	static int n;
-	static boolean[] visited;
-	static String[] arr;
-	static List<String> list = new ArrayList<>();
-	
-	static void dfs(String num, int idx) {
-		// 주어진 부등호의 모든 조건을 완성하면 리턴
-		if(idx == n+1) {
-			// 부등호가 성립되는 모든 경우의 수가 등록됨
-			list.add(num);
-			return;
-		}
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        k = Integer.parseInt(br.readLine());
+        inequality = br.readLine().replace(" ", "").toCharArray();
+        visited = new boolean[10];
+        arr = new int[k+1];
+    }
 
-		// 0~9 까지의 수
-		for(int j = 0 ; j < 10; j++) {
-			// 사용한 숫자인지 체크
-			if(visited[j]) {
-				continue;
-						
-			}
-			// Character.getNumericValue : char -> int 형으로 변환 (선택한 숫자)
-			// j , arr[idx-1] : 선택한 숫자에 비교할 숫자와, 비교할 부등호
-			if(idx == 0 || ckeck(Character.getNumericValue(num.charAt(idx - 1)), j , arr[idx-1])) {
-				visited[j] = true;
-				// true일시, num에 문자열 붙임
-				dfs(num+j, idx+1);
-				visited[j] = false;				
-			}
-		}
-		
-	}
-	
-	static boolean ckeck(int a, int b, String c) {
-		// 현재 사용하는 숫자와 j번째 숫자와 비교하여, 부등호가 성립되면 true
-		if (c.equals(">")) {
-			if(a < b) return false;
-		} else if (c.equals("<")){
-			if(a > b) return false;
-		}
-		return true;
-	}
+    static void rec(int count) {
+        // count == k+1 이면 수식이 완료
+        if(count == k+1) {
+            StringBuilder sb = new StringBuilder();
+            // 수식에 있는 숫자를 그대로 하나씩 꺼내서 StringBuilder에 붙이고
+            for (int i = 0; i < k+1; i++) { // 사용된 숫자만 추가
+                sb.append(arr[i]);
+            }
+            String num = sb.toString();
+            // 최댓값과 최솟값 갱신
+            if (num.compareTo(max) > 0) max = num;
+            if (num.compareTo(min) < 0) min = num;
+            return;
+        }
+        // base case를 충족하지 못한다면
+        // i는 수식에 들어갈 숫자
+        for(int i=0; i<10; i++) {
+            // i가 수식에 들어있지 않은 수라면
+            if(!visited[i]) {
+                // i가 checkCondition메서드에서 true를 받으면
+                if(count == 0 || checkCondition(arr[count-1], i, inequality[count-1])) {
+                    arr[count] = i;
+                    visited[i] = true;
+                    // 다음 위치로 진행
+                    rec(count+1);
+                    visited[i] = false;
+                }
+            }
+        }
+    }
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
-		arr = br.readLine().split(" ");
+    static boolean checkCondition(int prev, int now, char op) {
+        if(op == '<') {
+            return prev < now;
+        } else {
+            return prev > now;
+        }
+    }
 
-		visited = new boolean[10];
-		
-		// param1 : 리턴받을 문자열, param2: 인덱스(0부터시작)
-		dfs("",0);
-
-		//최대값
-		System.out.print(list.get(list.size() - 1) + "\n");
-		//최소값
-		System.out.print(list.get(0));
-		
-	}
-
+    public static void main(String[] args) throws IOException {
+        input();
+        rec(0);
+        System.out.println(max);
+        System.out.println(min);
+    }
 }
