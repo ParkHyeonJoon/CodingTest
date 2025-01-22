7f8cdec284948cb278c306f3307f52c48a843a44
parkhyeonjoon <97442408+ParkHyeonJoon@users.noreply.github.com>
2025-01-22 17:20:20 +0900

    [Silver I] Title: 단지번호붙이기, Time: 108 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,63 +1,58 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    static StringBuilder sb = new StringBuilder();
-    static int N, group_cnt;
-    static String[] a;
-    static boolean[][] visit;
-    static int[][] dir = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-    static ArrayList<Integer> group;
+    static int N;
+    static List<Integer> list = new ArrayList<>();
+    static boolean[][] visited;
+    static int[][] map;
+    static int[] dRow = {-1, 1, 0, 0};
+    static int[] dCol = {0, 0, -1, 1};
 
-    static void input() {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        sc.nextLine();
-        a = new String[N];
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        visited = new boolean[N][N];
+        map = new int[N][N];
         for(int i=0; i<N; i++) {
-            a[i] = sc.nextLine();
-        }
-        visit = new boolean[N][N];
-    }
-
-    // x,y를 갈 수 있다는 걸 알고 방문한 상태
-    static void dfs(int x, int y) {
-        // 단지에 속한 집의 개수 증가, visit 체크하기
-        group_cnt++;
-        visit[x][y] = true;
-
-        // 인접한 집으로 새로운 방문하기
-        for(int k=0; k<4; k++) {
-            int nx = x + dir[k][0];
-            int ny = y + dir[k][1];
-            if(nx < 0 || ny < 0 || ny >= N || nx >= N) continue;
-            if(a[nx].charAt(ny) == '0') continue;
-            if(visit[nx][ny]) continue;
-            dfs(nx, ny);
-        }
-    }
-
-    static void pro() {
-        group = new ArrayList<>();
-        for(int i=0; i<N; i++) {
+            String st = br.readLine();
             for(int j=0; j<N; j++) {
-                if(!visit[i][j] && a[i].charAt(j) == '1') {
-                    // 갈 수 있는 칸인데, 이미 방문처리 된, 즉 새롭게 만난 단지인 경우!
-                    group_cnt = 0;
-                    dfs(i, j);
-                    group.add(group_cnt);
+                map[i][j] = st.charAt(j) - '0';
+            }
+        }
+    }
+
+    static int dfs(int row, int col) {
+        visited[row][col] = true;
+        int count = 1;
+
+        for(int i=0; i<4; i++) {
+            int nextRow = row + dRow[i];
+            int nextCol = col + dCol[i];
+            if (nextRow >= 0 && nextRow < N && nextCol >= 0 && nextCol < N) {
+                if(!visited[nextRow][nextCol] && map[nextRow][nextCol] == 1) {
+                       count = count + dfs(nextRow, nextCol);
                 }
             }
         }
 
-        // 찾은 단지의 정보를 출력하기
-        Collections.sort(group);
-        sb.append(group.size()).append('\n');
-        for(int cnt : group) sb.append(cnt).append('\n');
-        System.out.println(sb.toString());
+        return count;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         input();
-        pro();
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<N; j++) {
+                if(!visited[i][j] && map[i][j] == 1) {
+                    list.add(dfs(i, j));
+                }
+            }
+        }
+        Collections.sort(list);
+        System.out.println(list.size());
+        for(int ans : list) {
+            System.out.println(ans);
+        }
+
     }
 }
